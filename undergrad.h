2e039ea0cfd943354b55314bdbaa4b4b9b16c229
zakariya0 <44619084+zakariya0@users.noreply.github.com>
2019-10-22 15:31:48 +1030

    Update undergrad.h

--- undergrad.h
+++ undergrad.h
@@ -3,9 +3,11 @@
 #include "user.h"
 
 class Undergrad : public User {
+    //the user class public attributes are inherited
     private:
 
     protected:
+        //these vectors are declared and can only be accessed by this class and those that inherit it
         vector<string> science_undergrad;
         vector<string> arts_undergrad;
         vector<string> law_undergrad;
@@ -15,9 +17,13 @@ class Undergrad : public User {
         vector<double> marks_undergrad;
 
     public:
+        //contains the default contructor
         Undergrad();
+        //contains the function that asks for information
         void personal_detail_prompt() override;
+        //asks which degree is to be selected
         void degree_prompt() override;
+        //returns true if marks are above threshold
         bool sufficient_marks() override;
 
 };
