314eeade930dc356e89eb84de67940617968e125
zakariya0 <44619084+zakariya0@users.noreply.github.com>
2019-10-22 15:27:47 +1030

    Update register_user.cpp

--- register_user.cpp
+++ register_user.cpp
@@ -5,13 +5,14 @@ using namespace std;
 
 bool Register_user(User& student)
 {
+    //function which asks for personal details and choices by calling functions
     bool sufficient_marks = true;
     
     student.personal_detail_prompt();
     student.facultyprompt();
     student.degree_prompt();
     sufficient_marks = student.sufficient_marks();
-
+    //checks if marks are sufficient by comparing to the sufficient marks and returns false if not
     if(!sufficient_marks)
     {
         sufficient_marks = false;
